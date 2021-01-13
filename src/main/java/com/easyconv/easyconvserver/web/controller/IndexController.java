90f687e3384790154d38871e2041b5b918552f3b
vitamaxDH <hellonewtry@gmail.com>
2021-01-14 02:50:08 +0900

    fix: 불필요한 import 제거

--- src/main/java/com/easyconv/easyconvserver/web/controller/IndexController.java
+++ src/main/java/com/easyconv/easyconvserver/web/controller/IndexController.java
@@ -1,10 +1,8 @@
 package com.easyconv.easyconvserver.web.controller;
 
-import com.easyconv.easyconvserver.core.conversion.service.PdfToHtmlConvertService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
