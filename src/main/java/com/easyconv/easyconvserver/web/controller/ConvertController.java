c698288637b0746925e39e4265f51c2c2decc4be
vitamaxDH <hellonewtry@gmail.com>
2021-01-17 02:48:20 +0900

    feat: 수정된 Pdf 서비스 적용

--- src/main/java/com/easyconv/easyconvserver/web/controller/ConvertController.java
+++ src/main/java/com/easyconv/easyconvserver/web/controller/ConvertController.java
@@ -1,22 +1,19 @@
 package com.easyconv.easyconvserver.web.controller;
 
-import com.easyconv.easyconvserver.config.ConfigUtil;
-import com.easyconv.easyconvserver.core.conversion.service.ToPdfConverService;
-import com.easyconv.easyconvserver.core.conversion.service.FromPdfConvertService;
-import com.easyconv.easyconvserver.core.resource.ResourceFile;
-import com.easyconv.easyconvserver.core.util.FileUtils;
-import com.easyconv.easyconvserver.core.util.GsonUtils;
+import com.easyconv.easyconvserver.core.conversion.service.PdfConvertService;
 import com.itextpdf.text.DocumentException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 @Slf4j
@@ -25,29 +22,13 @@ import java.io.IOException;
 @RequestMapping("/api/convert")
 public class ConvertController {
 
-    private final FromPdfConvertService fromPdfConvertService;
-    private final ToPdfConverService toPdfConverService;
-    private final ConfigUtil configUtil;
+    private final PdfConvertService pdfConvertService;
 
-    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> test(HttpServletRequest req, HttpServletResponse res, MultipartFile multipartFile) throws IOException, DocumentException {
-
-        log.info("type -> {}", FileUtils.tika.detect(multipartFile.getInputStream()));
-//        log.info("propertiesConfig.key -> {}", configUtil.getKey());
-        fromPdfConvertService.toImage(multipartFile);
-//        htmlToPdfConverService.convert(multipartFile);
-        return ResponseEntity.ok("OK");
-    }
-
-    @PostMapping({"","/"})
-    public String convert(@RequestBody String body, HttpServletRequest req, HttpServletResponse res) throws IOException, ParserConfigurationException {
-
-        ResourceFile resourceFile = GsonUtils.gson.fromJson(body, ResourceFile.class);
-
-        fromPdfConvertService.convert(resourceFile.getFilePath(), "D:/sample/output/test.html");
-
-        log.info("convert :: filePath -> {}", resourceFile.getFilePath());
-        return resourceFile.getFilePath();
+    @PostMapping(value = {"", "/"}, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ByteArrayResource> convertAndSend(MultipartFile multipartFile, HttpServletRequest req, HttpServletResponse res) throws IOException, DocumentException {
+        byte[] bytes = pdfConvertService.convertAndSend(multipartFile);
+        log.info("convertAndSend :: bytes -> {}", bytes);
+        return ResponseEntity.ok().body(new ByteArrayResource(bytes));
     }
 
 }
