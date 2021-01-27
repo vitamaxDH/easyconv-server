4c2c68426080ec25ace0d422c47d721fc7de2d04
vitamaxDH <hellonewtry@gmail.com>
2021-01-27 23:53:47 +0900

    feat: cors 설정 수정 변경 및 messageConverter 추가

--- src/main/java/com/easyconv/easyconvserver/config/WebConfig.java
+++ src/main/java/com/easyconv/easyconvserver/config/WebConfig.java
@@ -1,8 +1,13 @@
 package com.easyconv.easyconvserver.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.Charset;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -10,8 +15,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("Access-Control-Allow-Origin");
+                .allowedOrigins("http://localhost:3000", "*")
+                .allowedMethods("GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*");
     }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
 }
