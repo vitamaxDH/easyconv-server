c7b0a97434094758c07de279079750074a0ff3a7
vitamaxDH <hellonewtry@gmail.com>
2021-01-27 23:58:24 +0900

    feat: ms_office, charset value 추가

--- src/main/java/com/easyconv/easyconvserver/core/util/MimeTypeUtils.java
+++ src/main/java/com/easyconv/easyconvserver/core/util/MimeTypeUtils.java
@@ -6,9 +6,10 @@ public class MimeTypeUtils extends org.springframework.util.MimeTypeUtils {
     public static final String APPLICATION_VND_MSWORD_VALUE = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
     public static final String APPLICATION_VND_SPREADSHEETML_VALUE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
     public static final String APPLICATION_VND_ANY_MSEXCEL_VALUE = "application/vnd.com.documents4j.any-msexcel";
+    public static final String APPLICATION_X_TIKA_MSOFFICE = "application/x-tika-msoffice";
     public static final String APPLICATION_VND_MSEXCEL_VALUE = "application/vnd.ms-excel";
     public static final String TEXT_CSV_VALUE = "text/csv";
     public static final String APPLICATION_XHTML_XML_VALUE = "application/xhtml+xml";
     public static final String APPLICATION_PDF_VALUE = "application/pdf";
-
+    public static final String CHARSET_UTF_8 = "; charset=utf-8";
 }
