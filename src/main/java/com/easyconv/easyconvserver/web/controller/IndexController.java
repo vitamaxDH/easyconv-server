6ca8e1109f9dc3822070eb8a10a0adeef8e46796
vitamaxDH <hellonewtry@gmail.com>
2021-01-28 01:47:28 +0900

    feat: multipartFile null 조건 추가

--- src/main/java/com/easyconv/easyconvserver/web/controller/IndexController.java
+++ src/main/java/com/easyconv/easyconvserver/web/controller/IndexController.java
@@ -4,12 +4,12 @@ import com.easyconv.easyconvserver.core.conversion.service.PdfConvertService;
 import com.easyconv.easyconvserver.core.dto.GenericResourceDto;
 import com.easyconv.easyconvserver.core.util.WebUtils;
 import com.itextpdf.text.DocumentException;
-import com.sun.istack.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,13 +30,18 @@ public class IndexController {
         return "인덱스 페이지 입니다.";
     }
 
-    @PostMapping(value = "/convert", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = "application/octet-stream; charset=utf-8")
-    public ResponseEntity<GenericResourceDto> convertAndSend(@NotNull MultipartFile multipartFile, HttpServletRequest req, HttpServletResponse res) throws IOException, DocumentException {
+    @PostMapping(value = "/convert", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<GenericResourceDto> convertAndSend(@RequestBody MultipartFile multipartFile, HttpServletRequest req, HttpServletResponse res) throws IOException {
         GenericResourceDto dto = GenericResourceDto.create()
                                                     .setMultipartFile(multipartFile)
                                                     .setIp(WebUtils.getIp(req));
-        dto = pdfConvertService.convert(dto);
-        return ResponseEntity.ok(dto);
+        log.info("convertAndSend :: multipartFile {}", multipartFile);
+        if (ObjectUtils.isEmpty(multipartFile)){
+            return ResponseEntity.badRequest().body(dto);
+        } else {
+            dto = pdfConvertService.convert(dto);
+            return ResponseEntity.ok(dto);
+        }
     }
 
     @GetMapping(value = "/file/{fileId}")
