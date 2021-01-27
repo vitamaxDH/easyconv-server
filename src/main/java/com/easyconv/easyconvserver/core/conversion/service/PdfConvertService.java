25dc92644519f4001673d03b450608c7f863d92f
vitamaxDH <hellonewtry@gmail.com>
2021-01-27 23:54:54 +0900

    refact: convert => upload 만 담당하도록 변경

--- src/main/java/com/easyconv/easyconvserver/core/conversion/service/PdfConvertService.java
+++ src/main/java/com/easyconv/easyconvserver/core/conversion/service/PdfConvertService.java
@@ -3,18 +3,19 @@ package com.easyconv.easyconvserver.core.conversion.service;
 import com.documents4j.api.DocumentType;
 import com.documents4j.api.IConverter;
 import com.documents4j.job.LocalConverter;
+import com.easyconv.easyconvserver.core.dto.GenericResourceDto;
+import com.easyconv.easyconvserver.core.entity.ConvertHistory;
+import com.easyconv.easyconvserver.core.entity.PdfResource;
+import com.easyconv.easyconvserver.core.repository.ConvertHistoryRepository;
+import com.easyconv.easyconvserver.core.repository.PdfResourceRepository;
 import com.easyconv.easyconvserver.core.util.ExtensionType;
 import com.easyconv.easyconvserver.core.util.FileUtils;
 import com.easyconv.easyconvserver.core.util.MimeTypeUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.apache.commons.compress.utils.FileNameUtils;
 import org.springframework.stereotype.Service;
-
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
@@ -29,62 +30,67 @@ import static com.easyconv.easyconvserver.core.util.FileUtils.TIKA;
 @Service
 public class PdfConvertService implements Convertable {
 
-    @Override
-    public void convert(File inputFile) throws IOException {
-    }
+    private final PdfResourceRepository pdfResourceRepository;
+    private final ConvertHistoryRepository convertHistoryRepository;
 
     @Override
-    public void convert(MultipartFile inputFile) throws IOException {
+    public GenericResourceDto convert(GenericResourceDto dto) throws IOException {
+        return convert(dto, null);
+    }
+    @Override
+    public GenericResourceDto convert(GenericResourceDto dto, String outputPath) throws IOException {
+        log.info("convert :: START");
+        MultipartFile inputFile = dto.getMultipartFile();
+        File outputFile = this.getOutputFile(inputFile.getOriginalFilename(), outputPath);
+
+        boolean result = convert(inputFile.getInputStream(), outputFile);
+        if (result){
+            PdfResource resource = PdfResource.create().mapDtoToEntity(dto);
+            ConvertHistory convertHistory = ConvertHistory.builder()
+                                                          .clientIp(dto.getIp())
+                                                          .build();
+            pdfResourceRepository.save(resource);
+            convertHistoryRepository.save(convertHistory);
+        }
+        return dto;
     }
 
-    public ByteArrayResource convertAsByteArrayResource(MultipartFile inputFile) throws IOException {
-        return new ByteArrayResource(convertAsByteArray(inputFile));
+    private boolean convert(InputStream inputStream, String outputFilePath){
+        return convert(inputStream, new File(outputFilePath));
     }
 
-    public byte[] convertAsByteArray(MultipartFile inputFile) throws IOException {
-        File outputFile = FileUtils.createFile(inputFile.getName() + ExtensionType.PDF_WITH_DOT, Boolean.TRUE);
-        try  (
+    private boolean convert(InputStream inputStream, File outputFile){
+        boolean result = false;
+        try (
                 OutputStream outputStream = new FileOutputStream(outputFile);
         ){
-            DocumentType documentType = getDocumentType(inputFile.getInputStream());
+            DocumentType documentType = getDocumentType(inputStream);
             IConverter converter = LocalConverter.builder().build();
-            converter.convert(inputFile.getInputStream()).as(documentType).to(outputStream).as(DocumentType.PDF).execute();
+            result = converter.convert(inputStream)
+                    .as(documentType)
+                    .to(outputStream)
+                    .as(DocumentType.PDF)
+                    .execute();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return FileUtils.readFileToByteArray(outputFile);
+        return result;
     }
 
-    @Override
-    public void convert(String inputPath, String output) throws IOException {
-
+    private File getOutputFile(String fileName, String outputPath) throws IOException {
+        fileName = FileNameUtils.getBaseName(fileName);
+        if (ObjectUtils.isEmpty(outputPath)){
+            return FileUtils.createFile(fileName + ExtensionType.PDF_WITH_DOT.getValue(), Boolean.TRUE);
+        } else {
+            return FileUtils.createFile(outputPath);
+        }
     }
 
     public DocumentType getDocumentType(InputStream inputStream) throws  IOException {
+        // todo excel 은 다 안먹는것 같으니 다른 라이브러리를 찾아봐야 할 듯
+
         String mimeType = TIKA.detect(inputStream);
         return DocumentTypes.getDocumentType(mimeType);
-    }
-
-    public ResponseEntity<? extends Resource> getByteArrayResource(MultipartFile multipartFile) throws IOException {
-        Resource resource = convertAsByteArrayResource(multipartFile);
-
-        // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = TIKA.detect(multipartFile.getInputStream());
-        } catch (IOException ex) {
-            log.info("Could not determine file type.");
-        }
-
-        // Fallback to the default content type if type could not be determined
-        if(contentType == null) {
-            contentType = MimeTypeUtils.APPLICATION_OCTET_STREAM_VALUE;
-        }
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
     }
 
     enum DocumentTypes {
@@ -93,6 +99,7 @@ public class PdfConvertService implements Convertable {
         XLSX(MimeTypeUtils.APPLICATION_VND_SPREADSHEETML_VALUE, DocumentType.MS_EXCEL),
         EXCEL(MimeTypeUtils.APPLICATION_VND_ANY_MSEXCEL_VALUE, DocumentType.MS_EXCEL),
         XLS(MimeTypeUtils.APPLICATION_VND_MSEXCEL_VALUE, DocumentType.MS_EXCEL),
+        MSOFFICE(MimeTypeUtils.APPLICATION_X_TIKA_MSOFFICE, DocumentType.MS_EXCEL),
         CSV(MimeTypeUtils.TEXT_CSV_VALUE, DocumentType.CSV),
         AXML(MimeTypeUtils.APPLICATION_XML_VALUE, DocumentType.XML),
         TXML(MimeTypeUtils.TEXT_XML_VALUE, DocumentType.XML),
