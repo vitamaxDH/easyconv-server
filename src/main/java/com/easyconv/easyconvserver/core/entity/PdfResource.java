fbacac1bbbd6cd25aa10ab5a832cedb8e74964e1
vitamaxDH <hellonewtry@gmail.com>
2021-02-04 01:34:10 +0900

    feat: multipartFile -> file 로 수정

--- src/main/java/com/easyconv/easyconvserver/core/entity/PdfResource.java
+++ src/main/java/com/easyconv/easyconvserver/core/entity/PdfResource.java
@@ -1,13 +1,14 @@
 package com.easyconv.easyconvserver.core.entity;
 
-import com.easyconv.easyconvserver.config.Config;
 import com.easyconv.easyconvserver.core.dto.GenericResourceDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.File;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -20,7 +21,6 @@ public class PdfResource {
     @Id
     @Column
     private String id;
-
     @Column
     private String fileName;
     @Column
@@ -31,49 +31,18 @@ public class PdfResource {
     private String requestIp;
 
     @Column
-    private Instant createdTime;
+    private Instant createdTime = Instant.now();
 
     public static PdfResource create(){
         return new PdfResource();
     }
 
-    public PdfResource setId(String id) {
-        this.id = id;
-        return this;
-    }
-
-    public PdfResource setFileName(String fileName) {
-        this.fileName = fileName;
-        return this;
-    }
-
-    public PdfResource setParentPath(String parentPath) {
-        this.parentPath = parentPath;
-        return this;
-    }
-
-    public PdfResource setFullPath(String fullPath) {
-        this.fullPath = fullPath;
-        return this;
-    }
-
-    public PdfResource setRequestIp(String requestIp) {
-        this.requestIp = requestIp;
-        return this;
-    }
-
-    public PdfResource setCreatedTime(Instant createdTime) {
-        this.createdTime = createdTime;
-        return this;
-    }
-
     public PdfResource mapDtoToEntity(GenericResourceDto dto){
-        MultipartFile file = dto.getMultipartFile();
+        File file = dto.getConvertedFile();
         this.id = UUID.randomUUID().toString();
         this.fileName = file.getName();
-        this.parentPath = Config.getProperty("com.easyconv.pdf.file.output");
-        this.fullPath = this.parentPath + this.fileName;
-        this.createdTime = Instant.now();
+        this.parentPath = file.getParent();
+        this.fullPath = file.getAbsolutePath();
         return this;
     }
 }
