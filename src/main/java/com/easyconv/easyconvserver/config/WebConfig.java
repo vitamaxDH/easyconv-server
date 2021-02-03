78a6fae6459e38f7d215632ed5f6538ca864770e
vitamaxDH <hellonewtry@gmail.com>
2021-02-04 01:35:36 +0900

    chore: EnableWebMvc 추가

--- src/main/java/com/easyconv/easyconvserver/config/WebConfig.java
+++ src/main/java/com/easyconv/easyconvserver/config/WebConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.Charset;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
