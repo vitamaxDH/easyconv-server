074c445367de97d33f6276fc55193f3403199713
vitamaxDH <hellonewtry@gmail.com>
2021-02-04 01:31:32 +0900

    chore: xls, xlsx extension type 추가

--- src/main/java/com/easyconv/easyconvserver/core/util/ExtensionType.java
+++ src/main/java/com/easyconv/easyconvserver/core/util/ExtensionType.java
@@ -1,8 +1,9 @@
 package com.easyconv.easyconvserver.core.util;
 
 public enum ExtensionType {
-    PDF("pdf"),
-    PDF_WITH_DOT(".pdf"),
+    PDF("pdf"), PDF_WITH_DOT(".pdf"),
+    XLS("xls"), XLS_WITH_DOT(".xls"),
+    XLSX("xlsx"), XLSX_WITH_DOT(".xlsx"),
     ;
     private final String value;
 
