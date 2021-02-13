48739e111b05e6fb92c3e3d756c259571f40d128
vitamaxDH <hellonewtry@gmail.com>
2021-02-14 04:04:34 +0900

    feat: ConverterProvider 로 convert. 수정 중

--- src/main/java/com/easyconv/easyconvserver/core/util/FileUtils.java
+++ src/main/java/com/easyconv/easyconvserver/core/util/FileUtils.java
@@ -2,9 +2,12 @@ package com.easyconv.easyconvserver.core.util;
 
 import com.easyconv.easyconvserver.config.Config;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.compress.utils.FileNameUtils;
 import org.apache.tika.Tika;
 import org.apache.tika.io.FilenameUtils;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +15,7 @@ import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
+import java.util.UUID;
 
 @Slf4j
 public class FileUtils {
@@ -24,9 +28,9 @@ public class FileUtils {
     }
 
     public static File createFile(String filePath, boolean fromToday) throws IOException {
-        String tempPath = getBasePath();
+        String baseDir = getBasePath();
         Objects.requireNonNull(filePath);
-        File newFile = fromToday ? createFileToday(FilenameUtils.getName(filePath)) : new File(tempPath, filePath);
+        File newFile = fromToday ? createFileToday(FilenameUtils.getName(filePath)) : new File(baseDir, filePath);
         if (!newFile.exists()){
             Files.createDirectories(newFile.getParentFile().toPath());
         }
@@ -43,15 +47,54 @@ public class FileUtils {
         return org.apache.commons.io.FileUtils.readFileToByteArray(file);
     }
 
+    public static File getOutputFile(String outputPath, String fileName) throws IOException {
+        fileName = FileNameUtils.getBaseName(fileName);
+        if (ObjectUtils.isEmpty(outputPath)){
+            return createFile(fileName + ExtensionType.PDF_WITH_DOT.getValue(), Boolean.TRUE);
+        } else {
+            return createFile(outputPath);
+        }
+    }
+
     private static String getBasePath(){
         return Config.getProperty("com.easyconv.pdf.file.output");
     }
 
+    public static File multiFileToFile(MultipartFile multipartFile) throws IOException {
+        return multiFileToFile(multipartFile, null);
+    }
+
+    public static File multiFileToFile(MultipartFile multipartFile, String fileName) throws IOException {
+        File convFile;
+        if (!StringUtils.hasLength(fileName)){
+            convFile = new File(getBasePath(), UUID.randomUUID().toString());
+        } else {
+            convFile = createFileToday(fileName);
+        }
+        multipartFile.transferTo(convFile.toPath());
+        return convFile;
+    }
     public static boolean isExcel(String extension){
         if (!StringUtils.hasLength(extension)){
             log.warn("isExcel :: the given extension is null");
             return false;
         }
         return extension.contains(ExtensionType.XLS.getValue());
+    }
+
+    public static boolean isXls(String extension){
+        if (!StringUtils.hasLength(extension)){
+            log.warn("isXls :: the given extension is null");
+            return false;
+        }
+        return extension.endsWith(ExtensionType.XLS.getValue());
+    }
+
+    public static boolean isXlsx(String extension){
+        if (!StringUtils.hasLength(extension)){
+            log.warn("isXlsx :: the given extension is null");
+            return false;
+        }
+        return extension.endsWith(ExtensionType.XLSX.getValue());
     }
 }
--- src/main/java/com/easyconv/easyconvserver/web/controller/IndexController.java
+++ src/main/java/com/easyconv/easyconvserver/web/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.easyconv.easyconvserver.web.controller;
 import com.easyconv.easyconvserver.core.conversion.service.PdfConvertService;
 import com.easyconv.easyconvserver.core.dto.GenericResourceDto;
 import com.easyconv.easyconvserver.core.util.WebUtils;
-import com.itextpdf.text.DocumentException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
@@ -15,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Slf4j
 @RestController
