42688be6c8f50ce19c2ef11e0f1480ee8404313e
vitamaxDH <hellonewtry@gmail.com>
2021-03-08 02:05:01 +0900

    feat: ERole 구성 변경 및 get 메서드 추가

--- src/main/java/com/easyconv/easyconvserver/web/model/ERole.java
+++ src/main/java/com/easyconv/easyconvserver/web/model/ERole.java
@@ -1,6 +1,29 @@
 package com.easyconv.easyconvserver.web.model;
 
+import com.easyconv.easyconvserver.core.exception.RoleNotFoundException;
+import org.hibernate.annotations.NotFound;
+
 public enum ERole {
-    ROLE_USER,
-    ROLE_ADMIN
+    ROLE_USER("user"),
+    ROLE_ADMIN("admin");
+
+    private String roleStr;
+
+    ERole(String roleStr) {
+        this.roleStr = roleStr;
+    }
+
+    public String getRoleStr(){
+        return roleStr;
+    }
+
+    public static ERole getRoleStr(String role){
+        ERole result = null;
+        for(ERole eRole : values()){
+            if (eRole.getRoleStr().equals(role)){
+                result = eRole;
+            }
+        }
+        return result;
+    }
 }
