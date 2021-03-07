65fceefccc12c8d8e6c3902bb57ca5751ac6aaa8
vitamaxDH <hellonewtry@gmail.com>
2021-03-08 02:29:04 +0900

    fix: 빈 주입 실패하여 필드 주입으로 수정

--- src/main/java/com/easyconv/easyconvserver/web/securiuty/jwt/AuthTokenFilter.java
+++ src/main/java/com/easyconv/easyconvserver/web/securiuty/jwt/AuthTokenFilter.java
@@ -3,10 +3,10 @@ package com.easyconv.easyconvserver.web.securiuty.jwt;
 import com.easyconv.easyconvserver.web.auth.UserDetailsServiceImpl;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,34 +23,33 @@ import java.io.IOException;
 @Slf4j
 @NoArgsConstructor
 public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
     private JwtUtils jwtUtils;
+    @Autowired
     private UserDetailsServiceImpl userDetailsService;
-
-    public AuthTokenFilter(JwtUtils jwtUtils, UserDetailsServiceImpl userDetailsService) {
-        this.jwtUtils = jwtUtils;
-        this.userDetailsService = userDetailsService;
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        try {
-            String jwt = jwtUtils.parseJwt(request);
-            Jws<Claims> jws = jwtUtils.validateJwtToken(jwt);
-            if (jwt != null &&  jws!= null) {
-                String username = jwtUtils.getUserNameFromJwtToken(jws);
+        String url = request.getRequestURL().toString();
+        if (StringUtils.contains(url, "/signin")) {
+            try {
+                String jwt = jwtUtils.parseJwt(request);
+                Jws<Claims> jws = jwtUtils.validateJwtToken(jwt);
+                if (jwt != null && jws != null) {
+                    String username = jwtUtils.getUserNameFromJwtToken(jws);
 
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            } catch (Exception e) {
+                logger.error("Cannot set user authentication: {}", e);
             }
-        } catch (Exception e) {
-            logger.error("Cannot set user authentication: {}", e);
         }
-
         filterChain.doFilter(request, response);
     }
 }
