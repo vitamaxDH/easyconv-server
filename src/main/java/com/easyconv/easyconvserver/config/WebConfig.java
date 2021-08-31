b4b9d0bf356e115a65d42666c0d388c9a8696727
Daehan Choi <hellonewtry@gmail.com>
2021-09-01 01:41:44 +0900

    [feat] URL 로깅 인터셉터 추가

--- src/main/java/com/easyconv/easyconvserver/config/WebConfig.java
+++ src/main/java/com/easyconv/easyconvserver/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.easyconv.easyconvserver.config;
 
+import com.easyconv.easyconvserver.web.interceptor.UrlLoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.Charset;
@@ -13,6 +15,11 @@ import java.nio.charset.Charset;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UrlLoggingInterceptor());
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
---
+++ src/main/java/com/easyconv/easyconvserver/web/interceptor/UrlLoggingInterceptor.java
@@ -0,0 +1,25 @@
+package com.easyconv.easyconvserver.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class UrlLoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("[REQ URL] : {}", request.getRequestURL());
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+}
